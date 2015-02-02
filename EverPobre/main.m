7a09738d7767c6c8c14551b2e03a44e47cb4a306
david martínez ferrando <david@davidmartinezferrando.com>
2015-02-02 12:05:40 +0100

    Initial Commit

---
+++ EverPobre/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  EverPobre
+//
+//  Created by david martínez ferrando on 2/2/15.
+//  Copyright (c) 2015 david martínez ferrando. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
